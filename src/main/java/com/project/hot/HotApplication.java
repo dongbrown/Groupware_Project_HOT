9fe2c397a60fe15bb75ca22f7107eb52c346e3fc
Lunia1109 <164973128+Lunia1109@users.noreply.github.com>
2024-06-25 16:13:52 +0900

    MJ_First Commit for final Project

---
+++ src/main/java/com/project/hot/HotApplication.java
@@ -0,0 +1,13 @@
+package com.project.hot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotApplication.class, args);
+	}
+
+}
