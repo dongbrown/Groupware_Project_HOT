ac89fea3543fc2761c6f61775f191dfbcf6b960f
GD <GD@host.docker.internal>
2024-07-05 17:49:04 +0900

    GJH - pagebar 만드는 중

--- src/main/java/com/project/hot/employee/controller/EmployeeController.java
+++ src/main/java/com/project/hot/employee/controller/EmployeeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.project.hot.common.PageFactory;
 import com.project.hot.employee.model.dto.Department;
 import com.project.hot.employee.model.dto.Employee;
 import com.project.hot.employee.model.service.EmployeeService;
@@ -23,16 +22,14 @@ import lombok.RequiredArgsConstructor;
 public class EmployeeController {
 
 	private final EmployeeService service;
-	private final PageFactory page;
 
 	@GetMapping("/addressbook.do")
 	public String addressBook(@RequestParam(defaultValue = "1") int cPage, Model m) {
 		Map<String, Object> param=new HashMap<>();
 		param.put("cPage", cPage);
 		param.put("numPerpage", 12);
-		Map<String, Object> result=service.selectEmployees(param);
+		Map<String, Object> result=service.selectEmployeeList(param);
 		m.addAttribute("employees", (List<Employee>)result.get("employees"));
-		m.addAttribute("pagebar", page.getpage(cPage, 12, (int)result.get("totalData"), "addressbook.do"));
 		m.addAttribute("departments", (List<Department>)result.get("departments"));
 		return "employee/addressbook";
 	}
