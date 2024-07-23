025b3ae624ae370e89c9599d734b0b4b1615c75a
DESKTOP-TOJ0JQP\GD <dpdlxj12@gmail.com>
2024-07-23 09:22:18 +0900

    GJH - 조직도 완료

--- src/main/webapp/js/humanResource/orgChart.js
+++ src/main/webapp/js/humanResource/orgChart.js
@@ -8,112 +8,111 @@ $(document).ready(()=>{
 });
 
 //조직도 차트 그리기
-function makeOrgChart(orgData){
+function makeOrgChart(orgData) {
 
-      const options = {
-    contentKey: 'data',
-    width: 1100,
-    height: 700,
-    nodeWidth: 300,
-    nodeHeight: 120,
-    fontColor: '#000',
-    borderColor: '#333',
-    childrenSpacing: 50,
-    siblingSpacing: 20,
-    direction: 'top',
-    enableExpandCollapse: true,
-    nodeTemplate: (content) => {
-        if (content.dept != null) {
-            return `
+	const options = {
+		contentKey: 'data',
+		width: 1100,
+		height: 700,
+		nodeWidth: 300,
+		nodeHeight: 120,
+		fontColor: '#000',
+		borderColor: '#333',
+		childrenSpacing: 50,
+		siblingSpacing: 20,
+		direction: 'top',
+		enableExpandCollapse: true,
+		nodeTemplate: (content) => {
+			if (content.dept != null) {
+				return `
                 <div style="
-                    width: 100%; 
-                    height: 100%; 
-                    background-color: #fff; 
-                    border-radius: 5px; 
-                    display: flex; 
-                    align-items: center; 
-                    padding: 10px; 
-                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
-                    border: 2px solid ${options.borderColor};
-                ">
-                    <div style="
-                        width: 80px; 
-                        height: 80px; 
-                        background-color: #e0e0e0; 
-                        border-radius: 4px; 
-                        overflow: hidden; 
-                        margin-right: 10px; 
-                        display: flex; 
-                        align-items: center; 
-                        justify-content: center;
-                    ">
-                        <img src="${path+'/upload/employee/'+content.url}" alt="Profile Image" style="
-                            width: 100%; 
-                            height: 100%;
-                        "/>
-                    </div>
-                    <div style="
-                        display: flex; 
-                        flex-direction: column; 
-                        justify-content: center; 
-                        width: calc(100% - 90px);
-                    ">
-                        <div style="
-                            background-color: #c62828; 
-                            color: #fff; 
-                            font-size: 16px; 
-                            font-weight: bold; 
-                            text-align: center; 
-                            padding: 5px 0;
-                            margin-bottom: 10px;
-                            width: 100%;
-                        ">
-                            ${content.name}
-                        </div>
-                        <div style="
-                            font-size: 14px; 
-                            text-align: center; 
-                            margin-bottom: 5px;
-                        ">
-                            ${content.pos}
-                        </div>
-                        <div style="
-                            font-size: 12px; 
-                            text-align: center; 
-                            color: #757575;
-                        ">
-                            ${content.dept}
-                        </div>
-                    </div>
-                </div>
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+">
+    <div style="
+        width: 80px;
+        height: 80px;
+        background-color: #e0e0e0;
+        border-radius: 4px;
+        overflow: hidden;
+        margin-right: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    ">
+        <img src="${content.url != null ? path + '/upload/employee/' + content.url : path + '/images/undraw_profile.svg'}" alt="Profile Image" style="
+            width: 100%;
+            height: 100%;
+        "/>
+    </div>
+    <div style="
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: calc(100% - 90px);
+        height: 80px; /* 이미지 높이와 맞추기 위해 추가 */
+    ">
+        <div style="
+            background-color: #346aff;
+            color: #fff;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            padding: 1px 0;
+            margin-bottom: 10px;
+            width: 100%;
+        ">
+            ${content.name}
+        </div>
+        <div style="
+            font-size: 14px;
+            text-align: center;
+            margin-bottom: 5px;
+        ">
+            ${content.pos}
+        </div>
+        <div style="
+            font-size: 12px;
+            text-align: center;
+            color: #757575;
+        ">
+            ${content.dept}
+        </div>
+    </div>
+</div>
             `;
-        } else {
-            return `
+			} else {
+				return `
                 <div style="
-                    width: 100%; 
-                    height: 100%; 
-                    background-color: #fff; 
-                    border-radius: 8px; 
-                    display: flex; 
-                    align-items: center; 
-                    justify-content: center; 
-                    padding: 10px; 
-                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
-                    border: 2px solid ${options.borderColor};
+                    width: 100%;
+                    height: 100%;
+                    background-color: #fff;
+                    border-radius: 5px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    padding: 10px;
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
                     text-align: center;
-                    font-size: 16px;
+                    font-size: 24px;
                     font-weight: bold;
                 ">
                     ${content.name}
                 </div>
             `;
-        }
-    },
-        canvasStyle: 'border: 1px solid black;background: #f6f6f6;',
-        enableToolbar: true,
-      };
-      const tree = new ApexTree(document.getElementById('svg-tree'), options);
-      return tree.render(orgData);
+			}
+		},
+		canvasStyle: 'border: 1px solid black;background: #f6f6f6;',
+		enableToolbar: true,
+	};
+	const tree = new ApexTree(document.getElementById('svg-tree'), options);
+	return tree.render(orgData);
 }
 
 //사원, 부서 정보 가져오기
